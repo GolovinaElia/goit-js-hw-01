50b0bd96c4718295ad267d6ca3c4de24cb6531a5
GolovinaElia <eliagolovina@ukr.net>
2020-12-01 01:05:25 +0200

    Добавляет файлы

--- js/task-2.js
+++ js/task-2.js
@@ -22,3 +22,32 @@ if (total >= ordered) {
 } else {
     console.log('На складе недостаточно товаров!');
 }
+
+let total1 = 100;
+let ordered1 = 20;
+
+if (total1 >= ordered1) {
+    total1 = total1 - ordered1;
+    console.log('Заказ оформлен, с вами свяжется менеджер');
+} else {
+    console.log('На складе недостаточно товаров!');
+}
+
+let total2 = 100;
+let ordered2 = 80;
+
+if (total2 >= ordered2) {
+    total2 = total2 - ordered2;
+    console.log('Заказ оформлен, с вами свяжется менеджер');
+} else {
+    console.log('На складе недостаточно товаров!');
+}
+let total3 = 100;
+let ordered3 = 130;
+
+if (total3 >= ordered3) {
+    total3 = total3 - ordered3;
+    console.log('Заказ оформлен, с вами свяжется менеджер');
+} else {
+    console.log('На складе недостаточно товаров!');
+}
--- js/task-3.js
+++ js/task-3.js
@@ -10,3 +10,15 @@
 // const ADMIN_PASSWORD = 'jqueryismyjam';
 // let message;
 
+const ADMIN_PASSWORD = 'jqueryismyjam';
+let message = prompt('Введите пароль');
+console.log(message);
+if (message === null) {
+    message = 'Отменено пользователем!';
+}
+else if (message === ADMIN_PASSWORD) {
+    message = 'Добро пожаловать!';
+} else {
+    message = 'Доступ запрещен, неверный пароль!';
+    }  
+alert(message);
